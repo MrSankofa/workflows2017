dbb3d0a5727443b86a7d485d55c71c908f9008c0
Brett Cunningham <codesankofa@Bretts-MacBook-Pro.local>
2017-06-20 19:35:37 -0400

    See what changed. Inserted working code into gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,82 +2,75 @@ var gulp = require('gulp'),
     gutil = require('gulp-util'),
     coffee = require('gulp-coffee'),
     browserify = require('gulp-browserify'),
-    compass = require('gulp-compass'), //why didn't we add a gulp-sass?
+    compass = require('gulp-compass'),
     connect = require('gulp-connect'),
     concat = require('gulp-concat');
 
-var coffeeSources,
-  jsSources,
-  htmlSources,
-  jsonSources,
-  sassSources,
-  outputDir,
-  sassStyle;
+var env,
+    coffeeSources,
+    jsSources,
+    sassSources,
+    htmlSources,
+    jsonSources,
+    outputDir,
+    sassStyle;
 
-coffeeSources = ['components/coffee/tagline.coffee'];
-jsSources = [
-  'components/scripts/rclick.js',
-  'components/scripts/pixgrid.js',
-  'components/scripts/tagline.js',
-  'components/scripts/template.js'
-];
+env = process.env.NODE_ENV || 'development';
 
-htmlSources = ['outputDir' + '*.html'];
-jsonSources = ['outputDir' + 'js/*.json'];
-sassSources = ['components/sass/style.scss'];
-
-var env = process.env.NODE_ENV || 'development';
-
-if (env === 'development') {
+if (env==='development') {
   outputDir = 'builds/development/';
   sassStyle = 'compressed';
 } else {
   outputDir = 'builds/production/';
-  sassStyle = 'compressed';
+  sassStyle = 'nested';
 }
 
-gulp.task('json', function() {
-  gulp.src(jsonSources)
-  .pipe(connect.reload())
-});
 
-gulp.task('html', function() {
-  gulp.src('builds/development/*.html')
-  .pipe(connect.reload())
-});
+coffeeSources = ['components/coffee/*.coffee'];
+jsSources = [
+	'components/scripts/rclick.js',
+	'components/scripts/pixgrid.js',
+	'components/scripts/tagline.js',
+	'components/scripts/template.js'
 
-gulp.task('connect', function(){
-  connect.server({
-    root: outputDir,
-    livereload: true});
-});
+];
+
+sassSources = ['components/sass/*.scss'];
+htmlSources = [outputDir + '*.html'];
+jsonSources = [outputDir + '/js/*.json'];
 
 gulp.task('coffee', function() {
-  gulp.src(coffeeSources)
-    .pipe(coffee({ bare: true })
-      .on('error', gutil.log))
-    .pipe(gulp.dest('components/scripts'))
+	gulp.src(coffeeSources)
+	.pipe(coffee({ bare: true })
+	  .on('error', gutil.log))
+	.pipe(gulp.dest('components/scripts'))
 });
 
 gulp.task('js', function() {
-  gulp.src(jsSources)
-    .pipe(concat('script.js'))
-    .pipe(browserify())
-    .pipe(connect.reload())
-    .pipe(gulp.dest(outputDir + 'js'))
+	gulp.src(jsSources)
+		.pipe(concat('script.js'))
+		.pipe(browserify())
+		.pipe(gulp.dest(outputDir + 'js'))
+		.pipe(connect.reload())
+
 });
 
 gulp.task('compass', function() {
-  gulp.src(sassSources)
-    .pipe(compass({
-      sass: 'components/sass',
-      image: outputDir + 'images', //why is the images here?
-      style: 'compressed'
-    })
-    .on('error', gutil.log))
-    .pipe(connect.reload())
-    .pipe(gulp.dest(outputDir + 'css'))
+	gulp.src(sassSources)
+		.pipe(compass({
+			sass: 'components/sass',
+			css: outputDir + 'css',
+			image: outputDir + 'images',
+			style: sassStyle,
+			comments: true
+		}))
+		.pipe(connect.reload())
+		.on('error', gutil.log)
+		.pipe(gulp.dest(outputDir +'css'))
+
+
 });
+
 
 /*So this task 'watch' has a function to watch whatever is specified 
 using the gulp.watch method.
@@ -86,13 +79,30 @@ The method used here are watching the coffee script, javascript and sass
 sources, then each time in each method it calls for the corresponding task
 to be ran so that data is moved into development. This sets up live reloading.
  */
-gulp.task('watch', function() {
-  gulp.watch(coffeeSources, ['coffee']); //dest, redo command
-  gulp.watch(jsSources, ['js']);
-  gulp.watch('components/sass/*.scss', ['compass']);
-  gulp.watch('builds/development/*.html', ['html']);
-  gulp.watch(jsonSources, ['json']);
+gulp.task('watch', function(){
+	gulp.watch(coffeeSources, ['coffee']);
+	gulp.watch(jsSources, ['js']);
+	gulp.watch(sassSources, ['compass']);
+	gulp.watch(htmlSources, ['html']);
+	gulp.watch(jsonSources, ['json']);
+	
 });
 
-gulp.task('default', ['html', 'json', 'coffee', 'js', 'compass', 'connect', 'watch']);
+gulp.task('connect', function(){
+	connect.server({
+		root: outputDir,
+		livereload: true
+	});
+});
 
+gulp.task('html', function(){
+	gulp.src(htmlSources).pipe(connect.reload())
+
+});
+
+gulp.task('json', function() {
+  gulp.src(jsonSources)
+    .pipe(connect.reload())
+});
+
+gulp.task('default', ['html', 'coffee', 'js', 'compass', 'connect', 'watch']);
