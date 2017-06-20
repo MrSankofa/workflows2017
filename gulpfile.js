a213e9cb05c270588e18328ef3ba521e11159d2d
Brett Cunningham <codesankofa@Bretts-MacBook-Pro.local>
2017-06-20 15:53:20 -0400

    Created live reloading for HTML and JSON files

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,20 @@ var jsSources = [
   'components/scripts/tagline.js',
   'components/scripts/template.js'
 ];
+
+var htmlSources = ['builds/development/*.html'];
+var jsonSources = ['builds/development/js/*.json'];
 var sassSources = ['components/sass/style.scss'];
+
+gulp.task('json', function() {
+  gulp.src(jsonSources)
+  .pipe(connect.reload())
+});
+
+gulp.task('html', function() {
+  gulp.src(htmlSources)
+  .pipe(connect.reload())
+});
 
 gulp.task('connect', function(){
   connect.server({
@@ -60,7 +73,9 @@ gulp.task('watch', function() {
   gulp.watch(coffeeSources, ['coffee']); //dest, redo command
   gulp.watch(jsSources, ['js']);
   gulp.watch('components/sass/*.scss', ['compass']);
+  gulp.watch(htmlSources, ['html']);
+  gulp.watch(jsonSources, ['json']);
 });
 
-gulp.task('default', ['coffee', 'js', 'compass', 'connect', 'watch']);
+gulp.task('default', ['html', 'json', 'coffee', 'js', 'compass', 'connect', 'watch']);
 
